96a98c8620f2bf4a86cdfce5a69d4bdb4b86860b
Muhammad Anas <119598916+Anas546@users.noreply.github.com>
2024-06-02 14:37:49 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+
+
+
+let temperature = prompt("Enter Your City Temperature");
+temperature =Number(temperature);
+       if( temperature <= 60 && temperature >= 51) {
+    alert("It's very Dangerous Condition! Must be stop ") 
+
+} else if( temperature <= 50 && temperature >= 41) {
+    alert(" It's Heat Stroke situation! Don't go outside ")
+}
+ else if( temperature <= 40 && temperature >= 31) {
+    alert("To much hotness! Avoid going outside & stay cool")
+}
+ else if( temperature <= 30 && temperature >= 21) {
+    alert("Pleasant Weather! Go outside & enjoy This moments ")
+}
+ else if( temperature <= 20 && temperature >= 11) {
+    alert("It's winter situation! use's of jakets recomended")
+}
+ else if( temperature <= 10 && temperature >= 1) {
+    alert("It's Coolest situation! Keep wearing warm clothes")
+}
+ else if( temperature <= -1 && temperature >= -10) {
+    alert("It's very Dangerous Condition! Must be stop")
+}
